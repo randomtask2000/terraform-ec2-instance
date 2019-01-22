fb953d6a002fe31c28af2ec1372c67ee9a9eee4d
Emilio Nicoli <emilionicoli@gmail.com>
2019-01-22 10:27:57 -0700

    add instance profile

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Install terraform
-yum update -y
+sudo apt-get update -y >> /tmp/user_data.log
+sudo apt-get install git -y >> /tmp/user_data.log
 sudo apt-get install unzip -y >> /tmp/user_data.log
 wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip >> /tmp/user_data.log
 unzip terraform_0.11.10_linux_amd64.zip >> /tmp/user_data.log
 sudo mv terraform /usr/local/bin/ >> /tmp/user_data.log
 terraform --version >> /tmp/user_data.log
+sudo apt-get install awscli >> /tmp/user_data.log
--- prof.tf
+++ prof.tf
@@ -11,17 +11,17 @@ resource "aws_iam_role" "role" {
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-               "Service": "ec2.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 EOF
 }
